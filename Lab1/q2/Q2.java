7c68ee1792b734fc75bf9886ad1ef232f91b8839
phswarnasuriya <140691338+phswarnasuriya@users.noreply.github.com>
2023-07-31 13:32:48 +0530

    Add files via upload

---
+++ Lab1/q2/Q2.java
@@ -0,0 +1,12 @@
+
+package q2;
+
+public class Q2 {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Name: Pasindu");
+        System.out.println("Degree Programe: BSc Management Information System");
+    }
+    
+}
